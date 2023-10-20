8a669d335766ba1e9074cacfd082a113ca217f7c
foster-adu <adufoster1@yahoo.com>
2023-10-20 16:53:47 -0700

    C - Singly linked lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* @h: name of the list
+* print_list - check the code for     Holberton School students.
+* Return: the number of nodes printed.
+*/
+size_t print_list(const list_t *h)
+{
+int count = 0;
+
+while (h)
+{
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+{
+printf("[%d] %s\n", h->len, h->str);
+}
+count++;
+h = h->next;
+}
+return (count);
+}
+
