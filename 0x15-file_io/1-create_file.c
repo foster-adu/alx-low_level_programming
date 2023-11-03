6be0d7ae56df32548c6aa960bc3de271ace37614
foster-adu <adufoster1@yahoo.com>
2023-11-02 17:19:22 -0700

    C - File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,9 +3,9 @@
 /**
 * create_file - creates a file
 * @filename: filename.
-* @text_content: content writed in the file.
+* @text_content: content written in the file.
 *
-* Return: 1 if it success. -1 if it fails.
+* Return: 1 if successful. -1 if it fails.
 */
 int create_file(const char *filename, char *text_content)
 {
